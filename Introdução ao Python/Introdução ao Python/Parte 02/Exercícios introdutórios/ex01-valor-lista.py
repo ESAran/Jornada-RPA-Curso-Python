9be19c069af50a6352846035b56179214cb7f978
Eduardo Slomp Arán <105756006+ESAran@users.noreply.github.com>
2023-10-18 16:15:57 -0300

    Update ex01-valor-lista.py

--- Introdução ao Python/Introdução ao Python/Parte 02/Exercícios introdutórios/ex01-valor-lista.py
+++ Introdução ao Python/Introdução ao Python/Parte 02/Exercícios introdutórios/ex01-valor-lista.py
@@ -1,1 +1,17 @@
+lista = [1,2,9,14,30]
 
+
+# Versão destrinchada
+def valores_menores(listain, valor):
+  listafin = []
+  for item in listain:
+    if item < valor:
+      listafin.append(item)
+  
+  return listafin
+print(valores_menores(lista, 15))
+
+# Versão compacta
+def busca_list(lst, value):
+    return [x for x in lst if x < value]
+print(busca_list(lista, 15))
