9fd9371178e6b888230a3137c100bef4acce2694
Eduardo Slomp Arán <105756006+ESAran@users.noreply.github.com>
2023-10-20 13:58:11 -0300

    Update ex03-bubblesort.py

--- Introdução ao Python/Parte 02/Exercícios introdutórios/ex03-bubblesort.py
+++ Introdução ao Python/Parte 02/Exercícios introdutórios/ex03-bubblesort.py
@@ -1,11 +1,18 @@
+def bubblesort(vet):
+    for i in range(0, len(vet)-1) :
+        for j in range(0, len(vet)-1):
+            if vet[j] > vet[j + 1]:
+                aux = vet[j]
+                vet[j] = vet[j + 1]
+                vet[j + 1] = aux
+
+        print(str(i+1) + " - passo: " + str(vet))
+
+
 vet = [5, 4, 6, 2, 7, 4, 0, 3, 8, 1]
+vet2 = [50, 40, 30, 20, 10]
 
-for i in range(0, len(vet)-1) :
-    for j in range(0, len(vet)-1):
-        if vet[j] > vet[j + 1]:
-            aux = vet[j]
-            vet[j] = vet[j + 1]
-            vet[j + 1] = aux
-
-    print(str(i+1) + " - passo: " + str(vet))
+bubblesort(vet)
+print("")
+bubblesort(vet2)
     
