822ede52bbcdf9361065507135e8f12f4b7b8edd
Eduardo Slomp Arán <105756006+ESAran@users.noreply.github.com>
2024-03-27 10:54:01 -0300

    Update fake_data.py

--- Fundações/RPA Challenge/fake_data.py
+++ Fundações/RPA Challenge/fake_data.py
@@ -4,7 +4,7 @@ from file_manipulation import cria_csv, escreve_csv
 
 def fake_data():
     site_data = "https://www.fakenamegenerator.com/gen-random-br-br.php"
-    file_path = r'assets\challenge.xlsx'
+    file_path = r'assets\new_challenge.csv'
 
     driver = Browser.chrome_browser(site_data)
 
